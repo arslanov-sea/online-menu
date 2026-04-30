bc79f17d8e14ed63980e41ebe7ef5bfa1858f2ba
Arslanov Suleiman <103919339+arslanov-sea@users.noreply.github.com>
2026-05-01 00:16:11 +0300

    remove secret key from settings
    Replaced the secret key with a placeholder for security.

--- onlineMenu/onlineMenu/settings.py
+++ onlineMenu/onlineMenu/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ogkl0@1i7_%3-@93*xw0#@31l42!mir0tg_t!_j&tkdj=se8l('
+SECRET_KEY = 'replaced'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
